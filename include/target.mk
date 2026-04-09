4c14c5fbc70e758ff635978b323aac795a952d5c
fzs209 <75153914+fzs209@users.noreply.github.com>
2026-04-09 13:03:15 +0800

    Update target.mk

--- include/target.mk
+++ include/target.mk
@@ -57,7 +57,7 @@ DEFAULT_PACKAGES.router:=\
 	block-mount coremark kmod-nf-nathelper kmod-nf-nathelper-extra kmod-ipt-raw kmod-tun \
 	iptables-mod-tproxy iptables-mod-extra ipset ip-full default-settings luci luci-proto-ipv6 \
 	luci-app-arpbind luci-app-filetransfer luci-app-vsftpd \
-	luci-app-accesscontrol luci-app-turboacc luci-app-wol curl ca-certificates
+	luci-app-accesscontrol luci-app-turboacc luci-app-wol curl ca-certificates luci-app-nlbwmon
 
 ifneq ($(DUMP),)
   all: dumpinfo
